1c5a13f956ef7795e5cb8160572e1b61eb12e07c
Mauricio <maurolores1992@gmail.com>
2022-12-28 02:40:42 -0300

    agregando php

---
+++ enviarmensaje.php
@@ -0,0 +1,17 @@
+<?php
+
+$destino = "maurolores1992@gmail.com";
+$asunto = 'Mensaje de mauriciolores.com';
+
+$nombre = $_POST ['nombre'];
+$email = $_POST ['email'];
+$textarea = $_POST ['textarea'];
+
+$header = "Envio desde formulario de contacto de Fix it PC";
+$mensaje = "Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "\n" . "Mensaje : " . $textarea;
+
+mail($destino, $header, utf8_decode($mensaje));
+header('Location: exito.html');
+
+?>
+
