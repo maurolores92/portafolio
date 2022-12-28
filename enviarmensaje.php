0db8794d9413f78b7ae2e516b66061062168373a
Mauricio <maurolores1992@gmail.com>
2022-12-28 11:41:07 -0300

    agregando hoja php para volver a index

--- enviarmensaje.php
+++ enviarmensaje.php
@@ -7,7 +7,7 @@ $nombre = $_POST ['nombre'];
 $email = $_POST ['email'];
 $textarea = $_POST ['textarea'];
 
-$header = "Envio desde formulario de contacto de Fix it PC";
+$header = "Envio desde formulario de contacto de mauriciolores.com.ar";
 $mensaje = "Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "\n" . "Mensaje : " . $textarea;
 
 mail($destino, $header, utf8_decode($mensaje));
